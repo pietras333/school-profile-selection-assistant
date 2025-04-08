191145bf9e60fd2b1f797be88da7dbe1dc7f2a7b
Piotr Wendt <wendtpiotr333@gmail.com>
2025-04-08 21:00:55 +0200

    feat: configure gh pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: "/school-profile-selection-assistant/", // replace with your GitHub repo name
 });
